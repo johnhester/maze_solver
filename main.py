74b65a12a05ec0c8350bf9d981ee75900f2c7d31
John Hester <hesterusmaximus@gmail.com>
2024-01-12 20:59:44 -0600

    merge conflict

--- main.py
+++ main.py
@@ -13,13 +13,6 @@ p_e = Point(150, 240)
 p_f = Point(300, 187) 
 line = Line(point_a,point_b)
 line_b = Line(p_c, p_d)
-<<<<<<< HEAD
-line_c = Line(p_e, p_f)
-
-window.draw_line(line, 'red')
-window.draw_line(line_b, 'green')
-window.draw_line(line_c, 'blue')
-=======
 cell = Cell(200,200,300,300, window)
 cell2 = Cell(100,350,250,150, window)
 cell3 = Cell(100,350,250,150, window)
@@ -30,7 +23,6 @@ window.draw_line(line_b, 'green')
 cell.draw(200,200,250,150, 'blue')
 cell2.draw(250,250,300,200, 'purple')
 cell3.draw(300,200,350,150, 'orange')
->>>>>>> 738c109e08578867ec05e32f458e9db42da37e87
 window.wait_for_close()
 
 
