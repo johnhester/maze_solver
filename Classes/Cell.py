e7524b1fdfb52dbd962efb190a09f4f9ae14dafe
johnhester <jmhester31@gmail.com>
2024-01-24 12:18:42 -0600

    updated Cell class

--- Classes/Cell.py
+++ Classes/Cell.py
@@ -3,21 +3,28 @@ from Classes.Line import Line
 from Classes.Point import Point
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, win:Window, left_wall = True, right_wall = True, top_wall = True, bottom_wall = True) -> None:
+    def __init__(self,  win:Window, left_wall = True, right_wall = True, top_wall = True, bottom_wall = True) -> None:
         self.has_left_wall = left_wall
         self.has_right_wall = right_wall
         self.has_top_wall = top_wall 
         self.has_bottom_wall = bottom_wall
+        self._win = win
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+    def draw(self, x1, y1, x2, y2, fill_color):
+        #set Cell variables
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        self._win = win
-    def draw(self, fill_color):
+        #generate points for cell corners
         top_left = Point(self._x1, self._y1)
         bottom_right = Point(self._x2, self._y2)
         top_right = Point(self._x2, self._y1)
         bottom_left = Point(self._x1, self._y2)
+        #draw cell
         if self.has_left_wall:
             self._win.draw_line(Line(top_left, bottom_left), fill_color)
         if self.has_top_wall:
@@ -27,6 +34,7 @@ class Cell:
         if self.has_bottom_wall:
             self._win.draw_line(Line(bottom_right, bottom_left), fill_color)
     def draw_move(self, to_cell, undo=False):
+        
         line_color = 'red'
         if undo:
             line_color = 'gray'
